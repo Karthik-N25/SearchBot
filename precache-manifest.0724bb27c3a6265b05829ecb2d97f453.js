c00c0f1ddac35731e91bda60680c46b9327ff93c
karthik <n.karthikwellwisher@gmail.com>
2021-11-28 17:38:48 +0530

    Updates

---
+++ precache-manifest.0724bb27c3a6265b05829ecb2d97f453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dc9c25dadf7d5d041abaa44b474be5",
+    "url": "/SearchBot/index.html"
+  },
+  {
+    "revision": "9218183b449f5a51f20e",
+    "url": "/SearchBot/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "d55125f462fa179fc3c7",
+    "url": "/SearchBot/static/css/main.d7130983.chunk.css"
+  },
+  {
+    "revision": "9218183b449f5a51f20e",
+    "url": "/SearchBot/static/js/2.bcb0833c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/SearchBot/static/js/2.bcb0833c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55125f462fa179fc3c7",
+    "url": "/SearchBot/static/js/main.774d5a56.chunk.js"
+  },
+  {
+    "revision": "2d359705c283daa0d15a",
+    "url": "/SearchBot/static/js/runtime-main.742addda.js"
+  }
+]);
